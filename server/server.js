25bd44308e71011d6c57ff127794a4bd58f3a161
Hewah Bahrami <hewbahrami@gmail.com>
2020-08-18 19:40:05 -0400

    direct to correct endpoints and render service htmls

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ app.use(('/:id'), express.static(path.join(__dirname, '../client')));
 app.use('/:id/pictures/:id', createProxyMiddleware({ target: 'http://localhost:3000' }));
 app.use('/:id/similar/:id', createProxyMiddleware({ target: 'http://localhost:3001' }));
 // app.use('/:id/reviews', createProxyMiddleware({ target: 'http://localhost:3002' }));
-// app.use('/:id/bag/:id', createProxyMiddleware({ target: 'http://localhost:3003' }));
+app.use('/:id/bag/:id', createProxyMiddleware({ target: 'http://localhost:3003' }));
 
 
 
