14922d1bd2751e2e8c5b153521c9cac7b4a793a0
Hewah Bahrami <hewbahrami@gmail.com>
2020-08-19 13:18:52 -0400

    continued work on proxy

--- server/server.js
+++ server/server.js
@@ -7,13 +7,12 @@ const cors = require('cors');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 app.use(cors());
-// app.use(express.json());
 app.use(express.text());
 app.use(express.urlencoded({ extended: false }));
 app.use(('/:id'), express.static(path.join(__dirname, '../client')));
 app.use('/:id/pictures/:id', createProxyMiddleware({ target: 'http://localhost:3000' }));
 app.use('/:id/similar/:id', createProxyMiddleware({ target: 'http://localhost:3001' }));
-// app.use('/:id/reviews', createProxyMiddleware({ target: 'http://localhost:3002' }));
+// app.use('/reviews', createProxyMiddleware({ target: 'http://localhost:3002' }));
 app.use('/:id/bag/:id', createProxyMiddleware({ target: 'http://localhost:3003' }));
 
 
