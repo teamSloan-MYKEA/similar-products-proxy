c187e344ae493d19b6d8db695c995d6166d1a234
Hewah Bahrami <hewbahrami@gmail.com>
2020-08-22 13:16:34 -0400

    changed zach's service address

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.text());
 app.use(express.urlencoded({ extended: false }));
 app.use(('/:id'), express.static(path.join(__dirname, '../client')));
-app.use('/:id/pictures/:id', createProxyMiddleware({ target: 'http://18.190.28.33:3000' }));
+app.use('/:id/pictures/:id', createProxyMiddleware({ target: 'http://3.15.203.117:3000' }));
 app.use('/:id/similar/:id', createProxyMiddleware({ target: 'http://52.207.78.191' }));
 app.use('/:id/reviews/:id', createProxyMiddleware({ target: 'http://18.223.24.49:3002' }));
 // app.use('/:id/bag/:id', createProxyMiddleware({ target: 'http://localhost:3003' }));
