ecb1e927adc442e00c2e4dd35c0a731b3d63b029
Nathan Vu <mr.nathanvu@gmail.com>
2020-09-07 18:31:50 -0700

    proxy works for similar and pictures

--- server/server.js
+++ server/server.js
@@ -18,12 +18,15 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use(('/:id'), express.static(path.join(__dirname, '../client')));
 
-app.use('/:id/similar/:id', createProxyMiddleware({ target: 'http://localhost:3001' }));
+// app.use('/:id/similar/:id', createProxyMiddleware({ target: 'http://localhost:3001' }));
 // app.use('/:id/bag/:id', createProxyMiddleware({ target: 'http://localhost:3003' }));
 
 // app.use('/:id/pictures/:id', createProxyMiddleware({ target: 'http://3.15.203.117:3000' }));
 // app.use('/:id/similar/:id', createProxyMiddleware({ target: 'http://52.207.78.191' }));
 // app.use('/:id/reviews/:id', createProxyMiddleware({ target: 'http://18.223.24.49:3002' }));
+
+app.use('/:id/pictures/:id', createProxyMiddleware({ target: 'http://18.220.212.160:3000' }));
+app.use('/:id/similar/:id', createProxyMiddleware({ target: 'http://54.177.81.158:3001' }));
 
 
 app.listen(port, () => {
